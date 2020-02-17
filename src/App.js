74d0b79d530d32a01d52f886bb8e77832ded3b86
SAEBOMPAK <sbpark198405@gmail.com>
2020-02-18 08:03:44 +0900

    branch test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import Dashboard from './templates/dashboard';
 export default function App() {
   return (
     <div>
+      
+      {/* @TODO: Implement routing here
+      -> Create a route for SignIn
+      -> SignIn route should not be wrapped Dashboard component
+      -> Separate sidebar in a diferent component
+      -> Separate top bar in diferent component
+
+      (We want to have different templates per route, because Sign In should not render the dashboard template) */}
+
       <Dashboard>
         My content goes here
       </Dashboard>
